f417248a92cfd6d5c6c6926f829a9943cbf0d9a8
Mlb <97614168+devmlb@users.noreply.github.com>
2024-03-30 21:25:56 +0100

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-Hello World!
-<?php
-echo "Today is " . date("Y/m/d") . "<br>";
-echo "Today is " . date("Y.m.d") . "<br>";
-echo "Today is " . date("Y-m-d") . "<br>";
-echo "Today is " . date("l");
-?>
